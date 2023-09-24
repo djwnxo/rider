c7ed1f972ac959b95f6427a2b22ee13dc58a5666
GamerChris0837 <jjangga4227@gmail.com>
2023-09-24 15:40:07 +0900

    이ㅣㅈ안박멸기?

--- discord/iam.py
+++ discord/iam.py
@@ -1,132 +1,19 @@
 # 파이썬의 기본 내장 함수가 아닌 다른 함수 혹은 다른 기능이 필요할 때 사용함
 import discord, asyncio, datetime, pytz
-import yt_dlp as youtube_dl
+
 from json import loads
 from discord.ext import commands
 import requests
-TOKEN = 'MTE1MjgzMDAwMzQwNzUwMzQ1Mg.GM6pnh.svpCLHDBq1kOHoUTeENz_h7IKA2PHPS6TwE1X8'
+TOKEN = 'MTE1MjgzMDAwMzQwNzUwMzQ1Mg.GEqLUq.FcfHR8NfXUfX46wo1ki1obtEy4TUDGt96BGeYI'
 
-client = discord.Client(intents=discord.Intents.all(),command_prefix=commands.when_mentioned_or("/"))
+client = discord.Client(intents=discord.Intents.all())
 
-youtube_dl.utils.bug_reports_message = lambda: ''
 
-ytdl_format_options = {
-    'format': 'bestaudio/best',
-    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
-    'restrictfilenames': True,
-    'noplaylist': True,
-    'nocheckcertificate': True,
-    'ignoreerrors': False,
-    'logtostderr': False,
-    'quiet': True,
-    'no_warnings': True,
-    'default_search': 'auto',
-    'source_address': '0.0.0.0',  # bind to ipv4 since ipv6 addresses cause issues sometimes
-}
- 
-ffmpeg_options = {
-    'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
-    'options': '-vn',
-}
- 
-ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
- 
  
 # youtube 음악과 로컬 음악의 재생을 구별하기 위한 클래스 작성.
-class YTDLSource(discord.PCMVolumeTransformer):
-    def __init__(self, source, *, data, volume=0.5):
-        super().__init__(source, volume)
- 
-        self.data = data
- 
-        self.title = data.get('title')
-        self.url = data.get('url')
- 
-    @classmethod
-    async def from_url(cls, url, *, loop=None, stream=False):
-        loop = loop or asyncio.get_event_loop()
-        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
- 
-        if 'entries' in data:
-            # take first item from a playlist
-            data = data['entries'][0]
- 
-        filename = data['url'] if stream else ytdl.prepare_filename(data)
-        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
- 
- 
-# 음악 재생 클래스. 커맨드 포함.
 class Music(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
- 
-    @commands.command()
-    async def join(self, ctx, *, channel: discord.VoiceChannel):
-        """Joins a voice channel"""
- 
-        if ctx.voice_client is not None:
-            return await ctx.voice_client.move_to(channel)
- 
-        await channel.connect()
- 
-    @commands.command()
-    async def play(self, ctx, *, query):
-        """Plays a file from the local filesystem"""
- 
-        source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(query))
-        ctx.voice_client.play(source, after=lambda e: print(f'Player error: {e}') if e else None)
- 
-        await ctx.send(f'Now playing: {query}')
- 
-    @commands.command()
-    async def yt(self, ctx, *, url):
-        """Plays from a url (almost anything youtube_dl supports)"""
- 
-        async with ctx.typing():
-            player = await YTDLSource.from_url(url, loop=self.bot.loop)
-            ctx.voice_client.play(player, after=lambda e: print(f'Player error: {e}') if e else None)
- 
-        await ctx.send(f'Now playing: {player.title}')
- 
-    @commands.command()
-    async def stream(self, ctx, *, url):
-        """Streams from a url (same as yt, but doesn't predownload)"""
- 
-        async with ctx.typing():
-            player = await YTDLSource.from_url(url, loop=self.bot.loop, stream=True)
-            ctx.voice_client.play(player, after=lambda e: print(f'Player error: {e}') if e else None)
- 
-        await ctx.send(f'Now playing: {player.title}')
- 
-    @commands.command()
-    async def volume(self, ctx, volume: int):
-        """Changes the player's volume"""
- 
-        if ctx.voice_client is None:
-            return await ctx.send("Not connected to a voice channel.")
- 
-        ctx.voice_client.source.volume = volume / 100
-        await ctx.send(f"Changed volume to {volume}%")
- 
-    @commands.command()
-    async def stop(self, ctx):
-        """Stops and disconnects the bot from voice"""
- 
-        await ctx.voice_client.disconnect()
- 
-    @play.before_invoke
-    @yt.before_invoke
-    @stream.before_invoke
-    async def ensure_voice(self, ctx):
-        if ctx.voice_client is None:
-            if ctx.author.voice:
-                channel = ctx.message.author.voice.channel
-                await channel.connect
-            else:
-                await ctx.send("음성 채널에 접속하시지 않았습니다!")
-                raise commands.CommandError("Author not connected to a voice channel.")
-        elif ctx.voice_client.is_playing():
-            ctx.voice_client.stop()
     @commands.command()
     async def 명령어(self, ctx):
         await ctx.send("임베드 / 유튜브")
@@ -164,7 +51,7 @@ async def main():
 
 @client.event
 async def on_ready(): # 봇이 실행되면 한 번 실행됨
-    print("이 문장은 Python의 내장 함수를 출력하는 터미널에서 실행됩니다\n지금 보이는 것 처럼 말이죠")
+    print("Run.client")
     await client.change_presence(status=discord.Status.online, activity=discord.Game("Vscode"))
 
 @client.event
@@ -176,11 +63,13 @@ async def on_message(message):
     if message.content == "임베드": # 메세지 감지
         embed = discord.Embed(title="Example", description="example description",timestamp=datetime.datetime.now(pytz.timezone('UTC')), color=0xccf8ff)
 
-        embed.add_field(name="내가 고자라니", value="== 이지안", inline=False)
+        embed.add_field(name="ex", value="ex", inline=False)
 
         embed.set_footer(text="Bot Made by. KinJeok", icon_url="https://yt3.googleusercontent.com/D7et0EF7ez2G1Lpws8vQ12EJ9d0VR5eN6E2En6mjBOfpirlo1LqN6W-lqkce9fkUK2VHPCKh2g=s900-c-k-c0x00ffffff-no-rj")
         embed.set_thumbnail(url="https://yt3.googleusercontent.com/D7et0EF7ez2G1Lpws8vQ12EJ9d0VR5eN6E2En6mjBOfpirlo1LqN6W-lqkce9fkUK2VHPCKh2g=s900-c-k-c0x00ffffff-no-rj")
         await message.channel.send (embed=embed)
+        
+
 # 봇을 실행시키기 위한 토큰을 작성해주는 곳
 async def run_bot_and_main():
     await asyncio.gather(
@@ -191,3 +80,4 @@ async def run_bot_and_main():
 # Run the bot and the main function concurrently
 if __name__ == '__main__':
     asyncio.run(run_bot_and_main())
+    
--- discord/silhum.py
+++ discord/silhum.py
@@ -1,83 +1,80 @@
-@client.event
+import discord
+import asyncio
+import datetime
+import pytz
+from discord.ext import commands
 
+TOKEN = 'MTE1MjgzMDAwMzQwNzUwMzQ1Mg.GEqLUq.FcfHR8NfXUfX46wo1ki1obtEy4TUDGt96BGeYI'
+
+bot = commands.Bot(
+    command_prefix=commands.when_mentioned_or("/"),
+    description='Relatively simple music bot example',
+    intents=discord.Intents.all(),
+)
+
+class Music(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def 명령어(self, ctx):
+        await ctx.send("임베드 / 유튜브")
+
+    @commands.command()
+    async def 유튜브(self, ctx):
+        embed = discord.Embed(
+            title="KinJeok",
+            description="official youtube channel",
+            timestamp=datetime.datetime.now(pytz.timezone('UTC')),
+            color=0xccf8ff
+        )
+
+        embed.add_field(name="URL", value="https://www.youtube.com/channel/UC38G1mjjjGAJdRHbbbvIIUw", inline=False)
+
+        embed.set_footer(text="Bot Made by. KinJeok", icon_url="https://yt3.googleusercontent.com/D7et0EF7ez2G1Lpws8vQ12EJ9d0VR5eN6E2En6mjBOfpirlo1LqN6W-lqkce9fkUK2VHPCKh2g=s900-c-k-c0x00ffffff-no-rj")
+        embed.set_thumbnail(url="https://yt3.googleusercontent.com/D7et0EF7ez2G1Lpws8vQ12EJ9d0VR5eN6E2En6mjBOfpirlo1LqN6W-lqkce9fkUK2VHPCKh2g=s900-c-k-c0x00ffffff-no-rj")
+        await ctx.send(embed=embed)
+
+@bot.event
 async def on_ready():
+    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
+    print('------')
 
-    print(client.user.id)
+@bot.event
+async def on_message(message):
+    if '안녕' in message.content:
+        await message.channel.send("어서오세요! {}님!".format(message.author.mention))
+    if message.content == "납작복숭아":
+        await message.channel.send("뭐")
+    if message.content == "임베드":
+        embed = discord.Embed(
+            title="Example",
+            description="example description",
+            timestamp=datetime.datetime.now(pytz.timezone('UTC')),
+            color=0xccf8ff
+        )
 
-    print("ready")
+        embed.add_field(name="내가 고자라니", value="== 이지안", inline=False)
 
+        embed.set_footer(text="Bot Made by. KinJeok", icon_url="https://yt3.googleusercontent.com/D7et0EF7ez2G1Lpws8vQ12EJ9d0VR5eN6E2En6mjBOfpirlo1LqN6W-lqkce9fkUK2VHPCKh2g=s900-c-k-c0x00ffffff-no-rj")
+        embed.set_thumbnail(url="https://yt3.googleusercontent.com/D7et0EF7ez2G1Lpws8vQ12EJ9d0VR5eN6E2En6mjBOfpirlo1LqN6W-lqkce9fkUK2VHPCKh2g=s900-c-k-c0x00ffffff-no-rj")
+        await message.channel.send(embed=embed)
 
+@bot.command()
+async def 특별명령(ctx):
+    # 봇의 특별한 명령어 처리
+    await ctx.send("특별한 명령어를 실행합니다.")
 
-    # 디스코드 봇 상태 설정
+async def main():
+    bot.add_cog(Music(bot))
+    try:
+        await bot.start(TOKEN)
+    except KeyboardInterrupt:
+        await bot.close()
+        await bot.logout()
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
-    game = discord.Game(discord_bot_state)
-
-    await client.change_presence(status=discord.Status.online, activity=game)
-
-
-
-    # 채팅 채널 설정
-
-    channel = client.get_channel(discord_channelID)
-
-
-
-    # 트위치 api 2차인증
-
-    oauth_key = requests.post("https://id.twitch.tv/oauth2/token?client_id=" + twitch_Client_ID + "&client_secret=" + twitch_Client_secret + "&grant_type=client_credentials")
-
-    access_token = loads(oauth_key.text)["access_token"]
-
-    token_type = 'Bearer '
-
-    authorization = token_type + access_token
-
-    print(authorization)
-
-
-
-    check = False     #여기 오류를 수정합니다
-
-
-
-    while True:
-
-        print("ready on Notification")
-
-
-
-        # 트위치 api에게 방송 정보 요청
-
-        headers = {'client-id': twitch_Client_ID, 'Authorization': authorization}
-
-        response_channel = requests.get('https://api.twitch.tv/helix/streams?user_login=' + twitchID, headers=headers)
-
-        print(response_channel.text)
-
-        # 라이브 상태 체크
-
-        try:
-
-            # 방송 정보에서 'data'에서 'type' 값이 live 이고 체크상태가 false 이면 방송 알림(오프라인이면 방송정보가 공백으로 옴)
-
-            if loads(response_channel.text)['data'][0]['type'] == 'live' and check == False:
-
-                await channel.send(ment +'\n https://www.twitch.tv/' + twitchID)
-
-                print("Online")
-
-                check = True
-
-        except:
-
-            print("Offline")
-
-            check = False
-
-
-
-        await asyncio.sleep(30)
-
-
-
-client.run(discord_Token)
+# Run the bot and the main function concurrently
+if __name__ == '__main__':
+    asyncio.run(main())
